e711c836ea0e8900f0a1d3cdea5e5b66f5020715
Chrisplaysharp <cwalden@berkeley.edu>
2017-10-30 16:47:46 -0700

    heroku 3

--- config/connection.js
+++ config/connection.js
@@ -5,12 +5,11 @@ if(process.env.JAWSDB_URL) {
 	connection.mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 	connection = mysql.createConnection({
-	host: "localhost",
-	port: 3306,
-	user: "root",
- 	password: "",
-  	database: "burgers_db"
-});
+		host: "localhost",
+		user: "root",
+	 	password: "",
+	  	database: "burgers_db"
+	});
 }
 
 
