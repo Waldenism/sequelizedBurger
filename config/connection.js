2b7cfc06b158ee9040c17b0315107474233864b5
Chrisplaysharp <cwalden@berkeley.edu>
2017-10-30 18:08:52 -0700

    sisOHsix debug

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,14 @@ if(process.env.JAWSDB_URL) {
 } else {
 	connection = mysql.createConnection({
 		host: "localhost",
+		// port: 3306,
 		user: "root",
 	 	password: "",
 	  	database: "burgers_db"
 	});
 }
+
+
 
 connection.connect(function(err) {
   if (err) throw err;
